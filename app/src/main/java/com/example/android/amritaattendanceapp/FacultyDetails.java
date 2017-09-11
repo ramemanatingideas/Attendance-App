c6a6895f2897565faf10e51f02ba9c01694ea49c
Ram narayanan <[ramnaryanan51@gmail.com]>
2017-09-11 23:26:35 +0530

    created  new layout and intent

--- app/src/main/java/com/example/android/amritaattendanceapp/FacultyDetails.java
+++ app/src/main/java/com/example/android/amritaattendanceapp/FacultyDetails.java
@@ -1,13 +1,29 @@
 package com.example.android.amritaattendanceapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FacultyDetails extends AppCompatActivity {
+
+    private Button go_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_details);
+
+        go_back = (Button)findViewById(R.id.goBack);
+
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/android/amritaattendanceapp/MainActivity.java
+++ app/src/main/java/com/example/android/amritaattendanceapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.amritaattendanceapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtLaunchActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtLaunchActivity = (Button) findViewById(R.id.loginBTN);
+
+        mBtLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
     }
-}
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, FacultyDetails.class);
+        startActivity(intent);
+    }
+
+    }
+
+
+
+
